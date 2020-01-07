f1c97d3ce4320a117bfa83d0965195a3335dda04
brahmadaityo <53709038+brahmadaityo@users.noreply.github.com>
2020-01-07 18:40:43 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,27 +1,35 @@
 #include<stdio.h>
 #include<time.h>
+#define MAX(a,b) (a>b?a:b)
+void print(int mat[100][100],int m,int n)
+{
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("%d ",mat[i][j]);
+		}
+		printf("\n");
+	}
+}
 int main()
 {
-int i,j,k,m,n,;
-int mat[100][100],rmax[100],rmin[100],rmean[100],cmax[100],cmin[100],rmean[100];
+int i,j,k,m,n,min,max;
+float mean;
+int mat[100][100],rmax[100],rowmin[100],rowmean[100],colmax[100],colmin[100],colmean[100];
 srand(time(0));
 printf("enter the values of m,n,p\n");
-scanf("%d %d %d",&m,&n,&p);
+scanf("%d %d",&m,&n);
 for(i=0;i<m;i++)
 {
 	for(j=0;j<n;j++)
 	{
-		mat1[i][j][k]= (rand())%100; ///filling up the matrices
-		mat2[i][j][k]= (rand())%100;
+		mat1[i][j]= (rand())%100; ///filling up the matrices
 	}
 	
 }
-printf("the first matrix is \n");
-print(mat1,m,n,p);
-printf("the second matrix is \n");
-print(mat2,m,n,p);
-printf("the added matrix is \n");
-add(mat1,mat2,mat3,m,n,p);
+printf("the matrix is \n");
+print(mat,m,n);
 
 }
 
